6c4e43d1a625a5354065d4b2bc717051df0619a1
hellogaray <137473678+hellogaray@users.noreply.github.com>
2025-01-30 10:13:20 +0900

    exercise 5

---
+++ exercises/exercise_05.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import ttk
+
+def button_func(entry_string):
+    print('a button was pressed')
+    print(entry_string.get())
+# setup
+window = tk.Tk()
+window.title('buttons, functions, and arguments')
+
+# widgets
+entry_string = tk.StringVar(value = 'test')
+entry = ttk.Entry(window, textvariable = entry_string)
+entry.pack()
+
+button = ttk.Button(window, text = 'button', command = lambda: button_func(entry_string))
+button.pack()
+
+# run
+window.mainloop()
